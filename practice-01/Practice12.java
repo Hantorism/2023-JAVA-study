cb0e3a776f25ff788925fabd4284e8f487520934
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2023-03-14 13:39:06 +0900

    Add example

---
+++ practice-01/Practice12.java
@@ -0,0 +1,13 @@
+public class Practice12 {
+	public static void main(String[] args) {
+		String str = "012345678";
+        // TO DO : 
+		// 0번째 index부터 2(3-1)번째 index까지
+		// 1번째 index부터 문자열 끝까지
+	}
+}
+/*
+출력 결과
+012
+12345678
+*/
---
+++ practice-01/Practice13.java
@@ -0,0 +1,14 @@
+public class Practice13 {
+
+	public static void main(String[] args) {
+		String [] strs = {
+				"자바는", "이게", "되네"
+		};
+		// TO DO with for-loop
+
+		//
+	}
+}
+/*
+자바는! 이게! 되네!
+*/
