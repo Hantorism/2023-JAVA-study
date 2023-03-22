c5d3d47f4ee4aa9835c45fee1455e23f8b8a68a4
bigpie1367 <bigpie1367@gmail.com>
2023-03-22 15:08:40 +0900

    Add - practice 03

---
+++ practice-03/Practice04.java
@@ -0,0 +1,25 @@
+public class Practice04 {
+    public static void main(String[] args) {
+        // hint : Class 배열을 생성하는 방법
+        // ClassName[] arrayName = new ClassName[arrayLength];
+
+        // hint : 배열의 각 요소에 접근하는 방법
+        // arrayName[index] = ?
+
+        // hint : deck의 5장의 카드를 hand로 가져와야 한다
+
+        // hint : hand의 각 카드의 이름을 체크하는 메소드를 만들어보자
+
+    }
+
+    // hint : static draw method 선언부
+
+    {
+        // hint : deck의 첫 번째 요소를 hand로 옮기자
+
+        // hint : deck의 요소들을 다음 요소의 값으로 덮어씌우자
+        // 덮어씌우는 배열의 개수는 뽑힌 카드의 수만큼 감소해야 한다
+    }
+}
+
+// hint : class Card 선언부
