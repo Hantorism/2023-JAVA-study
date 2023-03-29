82611a3ae0f1fd5e3910cf74b25e0abaec6eac19
bigpie1367 <bigpie1367@gmail.com>
2023-03-29 13:35:48 +0900

    ADD - Practice 05

---
+++ practice-05/BeerTest.java
@@ -0,0 +1,29 @@
+class Beer {
+
+}
+
+class Cass extends Beer {
+
+}
+
+class Hoegaarden extends Beer {
+
+}
+
+public class BeerTest {
+    public static void main(String[] args) {
+        // TO DO
+
+        order(h1);
+        order(h2);
+        order(c);
+    }
+
+    static void order(Beer b) {
+        if (b instanceof Hoegaarden) {
+            // TO DO
+        } else if (b instanceof Cass) {
+            // TO DO
+        }
+    }
+}
