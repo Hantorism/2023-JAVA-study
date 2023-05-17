d8244a29e4d15986b389494aa6d806995755dd03
Sangjun Park <sangjun1389@ajou.ac.kr>
2023-05-17 18:42:03 +0900

    Fix practice-09 skeleton code

--- practice-09/Practice02_Generic2/PairTest.java
+++ practice-09/Practice02_Generic2/PairTest.java
@@ -18,14 +18,14 @@ public class PairTest {
     public static void main(String[] args) {
         System.out.println("--------Student--------");
         // TODO : 이름(본인 이름)과 학번(본인 학번)을 담을 수 있는 Pair 객체 studentInfo 생성
-        Pair<String, Integer> studentInfo = new Pair<>("김나박", 2020207361);
+        
         System.out.println("이름 : " + studentInfo.getKey());
         System.out.println("학번 : " + studentInfo.getValue());
 
         System.out.println();
         System.out.println("--------ID/PW--------");
         // TODO : ID(본인 학번)와 PW(아무 숫자)를 담을 수 있는 Pair 객체 IDPWInfo 생성
-        Pair<Integer, Integer> IDPWInfo = new Pair<>(2020207361, 1234);
+        
         System.out.println("ID : " + IDPWInfo.getKey());
         System.out.println("PW : " + IDPWInfo.getValue());
 
